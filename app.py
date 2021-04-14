2755d3e05a54cb2a62c174469575aad1af7caef5
Amir Siddiqui <62830615+siddiquiamir@users.noreply.github.com>
2021-04-14 12:05:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+# Load the csv file
+df = pd.read_csv("iris.csv")
+
+print(df.head())
+
+# Select independent and dependent variable
+X = df[["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]]
+y = df["Class"]
+
+# Split the dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=50)
+
+# Feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test= sc.transform(X_test)
+
+# Instantiate the model
+classifier = RandomForestClassifier()
+
+# Fit the model
+classifier.fit(X_train, y_train)
+
+# Make pickle file of our model
+pickle.dump(classifier, open("model.pkl", "wb"))
